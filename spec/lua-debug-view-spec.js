4acb5759f5a47626969c2c25ebf7ce43898949cd
Ju.chao <cqjcrom@gmail.com>
2016-08-24 09:52:41 +0800

    initial package.

---
+++ spec/lua-debug-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LuaDebugView from '../lib/lua-debug-view';
+
+describe('LuaDebugView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
